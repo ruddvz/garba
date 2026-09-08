d79ecb02af514371412c8c23ac0915bac18e95b6
ruddvz <pvr6675@gmail.com>
2026-09-07 22:42:16 -0400

    feat(player): load AutoMix audio compatibility runtime

--- simple-runtime.js
+++ simple-runtime.js
@@ -77,4 +77,4 @@
   else window.addEventListener('load', scheduleHydration, { once: true });
 })();
 
-document.write('<script src="provider-runtime.js"><\/script><script src="player-continuity.js"><\/script><script src="youtube-player-runtime.js"><\/script>');
+document.write('<script src="provider-runtime.js"><\/script><script src="player-continuity.js"><\/script><script src="assets/runtime/automix-web-audio.js"><\/script><script src="youtube-player-runtime.js"><\/script>');
