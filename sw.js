f73c55c5d02f81e75c0ae70e2fd25e987f906fea
ruddvz <pvr6675@gmail.com>
2026-09-08 02:35:58 -0400

    Package immersive atmosphere runtime

--- sw.js
+++ sw.js
@@ -18,6 +18,7 @@ const CORE_SHELL = [
   './catalogue/catalogue.js',
   './catalogue/listening-library.js',
   './assets/runtime/explore-search.js',
+  './assets/runtime/immersive-atmosphere.js',
   './manifest.webmanifest',
   './offline.html',
   './favicon.ico',
@@ -57,6 +58,7 @@ const FRESH_RUNTIME_SUFFIXES = [
   '/catalogue/catalogue.js',
   '/catalogue/listening-library.js',
   '/assets/runtime/explore-search.js',
+  '/assets/runtime/immersive-atmosphere.js',
 ];
 
 self.addEventListener('install', (event) => {
