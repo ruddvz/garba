0001338c088f756717d67e20924eb7eb5815357e
ruddvz <pvr6675@gmail.com>
2026-09-08 04:42:45 -0400

    Keep validated PWA cache generation

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'garba-live-';
-const CACHE_NAME = `${CACHE_PREFIX}v15`;
+const CACHE_NAME = `${CACHE_PREFIX}v14`;
 const LEGACY_PREFIX = 'garba-shell-';
 
 const CORE_SHELL = [
