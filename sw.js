60fc217c71faec07d12d33f22584a75867805e4a
ruddvz <pvr6675@gmail.com>
2026-09-07 23:16:54 -0400

    Complete cross-platform PWA icon precache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'garba-live-';
-const CACHE_NAME = `${CACHE_PREFIX}v11`;
+const CACHE_NAME = `${CACHE_PREFIX}v12`;
 const LEGACY_PREFIX = 'garba-shell-';
 
 const CORE_SHELL = [
@@ -13,11 +13,20 @@ const CORE_SHELL = [
   './nonstop-browser.js',
   './app.js',
   './manifest.webmanifest',
+  './browserconfig.xml',
   './offline.html',
+  './favicon.ico',
   './assets/icons/icon.svg',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png',
+  './assets/icons/favicon-16.png',
+  './assets/icons/favicon-32.png',
+  './assets/icons/favicon-48.png',
   './assets/icons/apple-touch-icon.png',
+  './assets/icons/apple-touch-icon-152.png',
+  './assets/icons/apple-touch-icon-167.png',
+  './assets/icons/mstile-150x150.png',
+  './assets/icons/mstile-310x310.png',
   './assets/icons/maskable.svg',
   './assets/icons/maskable-192.png',
   './assets/icons/maskable-512.png',
